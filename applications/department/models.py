d04fb3a4a640f6a00a81385dee210bd05ccbbcce
Fran Osores <fosores@linets.cl>
2022-09-17 19:25:05 -0300

    ADD initial model for department

--- applications/department/models.py
+++ applications/department/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    """Model definition for Department."""
+
+    name = models.CharField("Name", max_length=50)
+    short_name = models.CharField("Short Name", max_length=50)
+    is_active = models.BooleanField("Is Active", default=False)
+
+    class Meta:
+        """Meta definition for Department."""
+
+        verbose_name = 'Department'
+        verbose_name_plural = 'Departments'
+
+    def __str__(self):
+        """Unicode representation of Department."""
+        return self.name
+
+
