c779a60d5bd954eba62333ef4d9c1c0fe90fd2b3
Fran Osores <fosores@linets.cl>
2022-09-17 19:25:19 -0300

    ADD initial model for Employees

--- applications/employees/models.py
+++ applications/employees/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from applications.department.models import Department
+class Employee(models.Model):
+    """Model definition for employee"""
 
-# Create your models here.
+    JOB_CHOICES = (
+        ('0', 'Contador'),
+        ('1', 'Administrador'),
+        ('2', 'Economista'),
+        ('3', 'Otro')
+    )
+
+    first_name = models.CharField("Primer nombre", max_length=50)
+    last_name = models.CharField("Apellido", max_length=50)
+    position = models.CharField("Cargo", max_length=1, choices=JOB_CHOICES)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} -- {self.position}'
