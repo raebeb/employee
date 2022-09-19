b7d16931c1aee980cbc5dd3a97d3c9a14d0d66e3
Fran Osores <fosores@linets.cl>
2022-09-19 20:25:25 -0300

    add initial views

--- applications/employees/views.py
+++ applications/employees/views.py
@@ -1,3 +1,13 @@
+from curses.ascii import EM
 from django.shortcuts import render
+from django.views.generic import (
+    ListView, 
+)
 
-# Create your views here.
+from .models import Employee
+
+
+class ListAllEmployees(ListView):
+    template_name = 'employees/list_all_employees.html'
+    model = Employee
+    context_object_name = 'employees'
